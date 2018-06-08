0afbf96d76b2e6596a52856df20d72482a5b7af2
Emmanuel Semutenga <emzsema@gmail.com>
2018-06-08 14:38:37 +0300

    basic string formatting

---
+++ app.py
@@ -0,0 +1,25 @@
+#strings
+
+name = 'Semutenga' # single quotes
+
+name2 = "Semutenga Emmanuel" # double quotes
+
+phrase = 'Semutenga\'s world' #escaping string using the back slash
+
+phrase2 = 'Semutenga\'s world' #escaping string using double quotes
+
+s = 'First line.\nSecond line.'  # \n add a new line
+
+print(phrase)
+
+print(s)
+
+print(r'C:\some\name')  # using r before helps ignore the \ slash
+
+#for strings that span for more than one line use """___ """ or '''___'''
+
+print("""hey brother, the 
+is an endless road to \
+discover""")
+
+#forward slash will remove the new line when put at end of line
