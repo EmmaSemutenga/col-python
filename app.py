c02e5483752ce9537b021464a2fa3d12a2df4a99
Emmanuel Semutenga <emzsema@gmail.com>
2018-06-08 20:42:30 +0300

    basic string formatting finished

--- app.py
+++ app.py
@@ -23,3 +23,29 @@ is an endless road to \
 discover""")
 
 #forward slash will remove the new line when put at end of line
+
+#string concatenation and repeation
+
+print(3 * 'un' + 'ium')
+
+#Two or more string literals next to each other are automatically concatenated, only works with two literals, used to break long strings
+
+print('Py' 'thon')
+
+#Strings can be indexed with the first character having index 0
+
+word = 'Python'
+print(word[0])  # character in position 0
+
+print(word[-1])  # last character
+
+#obtaining substrings by slicing
+print(word[0:2])  # characters from position 0 (included) to 2 (excluded) 
+print(word)
+print(word[2:len(word)])  # characters from position 2 (included) to 5 (excluded)
+
+print(word[:2] + word[2:]) # is equal to Python
+
+#Python strings cannot be changed — they are immutable
+
+#built-in function len() returns the length of a string
